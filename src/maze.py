762b73555a2e27c2138d227e574ba18f48f78ee6
Neil Hemmerly <neilhemm0@gmail.com>
2025-01-28 16:27:33 -0500

    work on Maze class

--- src/maze.py
+++ src/maze.py
@@ -1,3 +1,4 @@
+import time
 from cell import Cell
 
 class Maze():
@@ -21,4 +22,24 @@ class Maze():
         self._cells = []
 
     def _create_cell(self):
-        self._cells.append([[Cell(self.win, )]])
+        position_x = self.x1
+        position_y = self.y1
+        for col in range(self.num_cols):
+            self._cells.append([])
+            for row in range(self.num_rows):
+                self._cells[col].append(Cell(self.win.canvas, position_x, position_x + self.cell_size_x, position_y, position_y + self.cell_size_y))
+                position_x += self.cell_size_x
+            position_y += self.cell_size_y
+        for i in range(len(self._cells)):
+            for j in range(len(self._cels[i])):
+                self._draw_cell(i, j)
+        
+    
+    def _draw_cell(self, i, j):
+        cell_position = (i * self.cell_size_x, j * self.cell_size_y)
+        cell_size = (self.cell_size_x * self.cell_size_y)
+        maze_position = (self.x1, self.y1)
+
+    def _animate(self):
+        self.win.redraw()
+        
